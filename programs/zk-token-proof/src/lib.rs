2584ae6ebb4e027450661b9fbfc65b897ddf1078
convowork1 <vladyslav.v@blaize.tech>
2023-08-14 17:13:25 +0300

    Inital commit of mainnet version 1.14.20 with some minor changes due fork

---
+++ programs/zk-token-proof/src/lib.rs
@@ -0,0 +1,171 @@
+#![forbid(unsafe_code)]
+
+use {
+    bytemuck::Pod,
+    solana_program_runtime::{ic_msg, invoke_context::InvokeContext},
+    solana_sdk::{
+        instruction::{InstructionError, TRANSACTION_LEVEL_STACK_HEIGHT},
+        system_program,
+    },
+    solana_zk_token_sdk::{
+        zk_token_proof_instruction::*,
+        zk_token_proof_program::id,
+        zk_token_proof_state::{ProofContextState, ProofContextStateMeta},
+    },
+    std::result::Result,
+};
+
+fn process_verify_proof<T, U>(invoke_context: &mut InvokeContext) -> Result<(), InstructionError>
+where
+    T: Pod + ZkProofData<U>,
+    U: Pod,
+{
+    let transaction_context = &invoke_context.transaction_context;
+    let instruction_context = transaction_context.get_current_instruction_context()?;
+    let instruction_data = instruction_context.get_instruction_data();
+    let proof_data = ProofInstruction::proof_data::<T, U>(instruction_data).ok_or_else(|| {
+        ic_msg!(invoke_context, "invalid proof data");
+        InstructionError::InvalidInstructionData
+    })?;
+
+    proof_data.verify_proof().map_err(|err| {
+        ic_msg!(invoke_context, "proof_verification failed: {:?}", err);
+        InstructionError::InvalidInstructionData
+    })?;
+
+    // create context state if accounts are provided with the instruction
+    if instruction_context.get_number_of_instruction_accounts() > 0 {
+        let context_state_authority = *instruction_context
+            .try_borrow_instruction_account(transaction_context, 1)?
+            .get_key();
+
+        let mut proof_context_account =
+            instruction_context.try_borrow_instruction_account(transaction_context, 0)?;
+
+        if *proof_context_account.get_owner() != id() {
+            return Err(InstructionError::InvalidAccountOwner);
+        }
+
+        let proof_context_state_meta =
+            ProofContextStateMeta::try_from_bytes(proof_context_account.get_data())?;
+
+        if proof_context_state_meta.proof_type != ProofType::Uninitialized.into() {
+            return Err(InstructionError::AccountAlreadyInitialized);
+        }
+
+        let context_state_data = ProofContextState::encode(
+            &context_state_authority,
+            T::PROOF_TYPE,
+            proof_data.context_data(),
+        );
+
+        if proof_context_account.get_data().len() != context_state_data.len() {
+            return Err(InstructionError::InvalidAccountData);
+        }
+
+        proof_context_account.set_data(&context_state_data)?;
+    }
+
+    Ok(())
+}
+
+fn process_close_proof_context(invoke_context: &mut InvokeContext) -> Result<(), InstructionError> {
+    let transaction_context = &invoke_context.transaction_context;
+    let instruction_context = transaction_context.get_current_instruction_context()?;
+
+    let owner_pubkey = {
+        let owner_account =
+            instruction_context.try_borrow_instruction_account(transaction_context, 2)?;
+
+        if !owner_account.is_signer() {
+            return Err(InstructionError::MissingRequiredSignature);
+        }
+        *owner_account.get_key()
+    }; // done with `owner_account`, so drop it to prevent a potential double borrow
+
+    let proof_context_account_pubkey = *instruction_context
+        .try_borrow_instruction_account(transaction_context, 0)?
+        .get_key();
+    let destination_account_pubkey = *instruction_context
+        .try_borrow_instruction_account(transaction_context, 1)?
+        .get_key();
+    if proof_context_account_pubkey == destination_account_pubkey {
+        return Err(InstructionError::InvalidInstructionData);
+    }
+
+    let mut proof_context_account =
+        instruction_context.try_borrow_instruction_account(transaction_context, 0)?;
+    let proof_context_state_meta =
+        ProofContextStateMeta::try_from_bytes(proof_context_account.get_data())?;
+    let expected_owner_pubkey = proof_context_state_meta.context_state_authority;
+
+    if owner_pubkey != expected_owner_pubkey {
+        return Err(InstructionError::InvalidAccountOwner);
+    }
+
+    let mut destination_account =
+        instruction_context.try_borrow_instruction_account(transaction_context, 1)?;
+    destination_account.checked_add_lamports(proof_context_account.get_lamports())?;
+    proof_context_account.set_lamports(0)?;
+    proof_context_account.set_data_length(0)?;
+    proof_context_account.set_owner(system_program::id().as_ref())?;
+
+    Ok(())
+}
+
+pub fn process_instruction(
+    _first_instruction_account: usize,
+    invoke_context: &mut InvokeContext,
+) -> Result<(), InstructionError> {
+    if invoke_context.get_stack_height() != TRANSACTION_LEVEL_STACK_HEIGHT {
+        // Not supported as an inner instruction
+        return Err(InstructionError::UnsupportedProgramId);
+    }
+
+    // Consume compute units since proof verification is an expensive operation
+    {
+        let compute_meter = invoke_context.get_compute_meter();
+        // TODO: Tune the number of units consumed.  The current value is just a rough estimate
+        compute_meter.borrow_mut().consume(100_000)?;
+    }
+
+    let transaction_context = &invoke_context.transaction_context;
+    let instruction_context = transaction_context.get_current_instruction_context()?;
+    let instruction_data = instruction_context.get_instruction_data();
+    let instruction = ProofInstruction::instruction_type(instruction_data)
+        .ok_or(InstructionError::InvalidInstructionData)?;
+
+    match instruction {
+        ProofInstruction::CloseContextState => {
+            ic_msg!(invoke_context, "CloseContextState");
+            process_close_proof_context(invoke_context)
+        }
+        ProofInstruction::VerifyZeroBalance => {
+            ic_msg!(invoke_context, "VerifyCloseAccount");
+            process_verify_proof::<ZeroBalanceProofData, ZeroBalanceProofContext>(invoke_context)
+        }
+        ProofInstruction::VerifyWithdraw => {
+            ic_msg!(invoke_context, "VerifyWithdraw");
+            process_verify_proof::<WithdrawData, WithdrawProofContext>(invoke_context)
+        }
+        ProofInstruction::VerifyCiphertextCiphertextEquality => {
+            ic_msg!(invoke_context, "VerifyWithdrawWithheldTokens");
+            process_verify_proof::<
+                CiphertextCiphertextEqualityProofData,
+                CiphertextCiphertextEqualityProofContext,
+            >(invoke_context)
+        }
+        ProofInstruction::VerifyTransfer => {
+            ic_msg!(invoke_context, "VerifyTransfer");
+            process_verify_proof::<TransferData, TransferProofContext>(invoke_context)
+        }
+        ProofInstruction::VerifyTransferWithFee => {
+            ic_msg!(invoke_context, "VerifyTransferWithFee");
+            process_verify_proof::<TransferWithFeeData, TransferWithFeeProofContext>(invoke_context)
+        }
+        ProofInstruction::VerifyPubkeyValidity => {
+            ic_msg!(invoke_context, "VerifyPubkeyValidity");
+            process_verify_proof::<PubkeyValidityData, PubkeyValidityProofContext>(invoke_context)
+        }
+    }
+}
---
+++ zk-token-sdk/src/zk_token_proof_instruction.rs
@@ -0,0 +1,257 @@
+///! Instructions provided by the ZkToken Proof program
+pub use crate::instruction::*;
+use {
+    bytemuck::bytes_of,
+    num_derive::{FromPrimitive, ToPrimitive},
+    num_traits::{FromPrimitive, ToPrimitive},
+    solana_program::{
+        instruction::{AccountMeta, Instruction},
+        pubkey::Pubkey,
+    },
+};
+
+#[derive(Clone, Copy, Debug, FromPrimitive, ToPrimitive, PartialEq, Eq)]
+#[repr(u8)]
+pub enum ProofInstruction {
+    /// Close a zero-knowledge proof context state.
+    ///
+    /// Accounts expected by this instruction:
+    ///   0. `[writable]` The proof context account to close
+    ///   1. `[writable]` The destination account for lamports
+    ///   2. `[signer]` The context account's owner
+    ///
+    /// Data expected by this instruction:
+    ///   None
+    ///
+    CloseContextState,
+
+    /// Verify a zero-balance proof.
+    ///
+    /// This instruction can be configured to optionally create a proof context state account.
+    ///
+    /// Accounts expected by this instruction:
+    ///
+    ///   * Creating a proof context account
+    ///   0. `[writable]` The proof context account
+    ///   1. `[]` The proof context account owner
+    ///
+    ///   * Otherwise
+    ///   None
+    ///
+    /// Data expected by this instruction:
+    ///   `ZeroBalanceProofData`
+    ///
+    VerifyZeroBalance,
+
+    /// Verify a withdraw zero-knowledge proof.
+    ///
+    /// This instruction can be configured to optionally create a proof context state account.
+    ///
+    /// Accounts expected by this instruction:
+    ///
+    ///   * Creating a proof context account
+    ///   0. `[writable]` The proof context account
+    ///   1. `[]` The proof context account owner
+    ///
+    ///   * Otherwise
+    ///   None
+    ///
+    /// Data expected by this instruction:
+    ///   `WithdrawData`
+    ///
+    VerifyWithdraw,
+
+    /// Verify a ciphertext-ciphertext equality proof.
+    ///
+    /// This instruction can be configured to optionally create a proof context state account.
+    ///
+    /// Accounts expected by this instruction:
+    ///
+    ///   * Creating a proof context account
+    ///   0. `[writable]` The proof context account
+    ///   1. `[]` The proof context account owner
+    ///
+    ///   * Otherwise
+    ///   None
+    ///
+    /// Data expected by this instruction:
+    ///   `CiphertextCiphertextEqualityProofData`
+    ///
+    VerifyCiphertextCiphertextEquality,
+
+    /// Verify a transfer zero-knowledge proof.
+    ///
+    /// This instruction can be configured to optionally create a proof context state account.
+    ///
+    /// Accounts expected by this instruction:
+    ///
+    ///   * Creating a proof context account
+    ///   0. `[writable]` The proof context account
+    ///   1. `[]` The proof context account owner
+    ///
+    ///   * Otherwise
+    ///   None
+    ///
+    /// Data expected by this instruction:
+    ///   `TransferData`
+    ///
+    VerifyTransfer,
+
+    /// Verify a transfer with fee zero-knowledge proof.
+    ///
+    /// This instruction can be configured to optionally create a proof context state account.
+    ///
+    /// Accounts expected by this instruction:
+    ///
+    ///   * Creating a proof context account
+    ///   0. `[writable]` The proof context account
+    ///   1. `[]` The proof context account owner
+    ///
+    ///   * Otherwise
+    ///   None
+    ///
+    /// Data expected by this instruction:
+    ///   `TransferWithFeeData`
+    ///
+    VerifyTransferWithFee,
+
+    /// Verify a pubkey validity zero-knowledge proof.
+    ///
+    /// This instruction can be configured to optionally create a proof context state account.
+    ///
+    /// Accounts expected by this instruction:
+    ///
+    ///   * Creating a proof context account
+    ///   0. `[writable]` The proof context account
+    ///   1. `[]` The proof context account owner
+    ///
+    ///   * Otherwise
+    ///   None
+    ///
+    /// Data expected by this instruction:
+    ///   `PubkeyValidityData`
+    ///
+    VerifyPubkeyValidity,
+}
+
+/// Pubkeys associated with a context state account to be used as parameters to functions.
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct ContextStateInfo<'a> {
+    pub context_state_account: &'a Pubkey,
+    pub context_state_authority: &'a Pubkey,
+}
+
+/// Create a `CloseContextState` instruction.
+pub fn close_context_state(
+    context_state_info: ContextStateInfo,
+    destination_account: &Pubkey,
+) -> Instruction {
+    let accounts = vec![
+        AccountMeta::new(*context_state_info.context_state_account, false),
+        AccountMeta::new(*destination_account, false),
+        AccountMeta::new_readonly(*context_state_info.context_state_authority, true),
+    ];
+
+    let data = vec![ToPrimitive::to_u8(&ProofInstruction::CloseContextState).unwrap()];
+
+    Instruction {
+        program_id: crate::zk_token_proof_program::id(),
+        accounts,
+        data,
+    }
+}
+
+/// Create a `VerifyZeroBalance` instruction.
+pub fn verify_zero_balance(
+    context_state_info: Option<ContextStateInfo>,
+    proof_data: &ZeroBalanceProofData,
+) -> Instruction {
+    ProofInstruction::VerifyZeroBalance.encode_verify_proof(context_state_info, proof_data)
+}
+
+/// Create a `VerifyWithdraw` instruction.
+pub fn verify_withdraw(
+    context_state_info: Option<ContextStateInfo>,
+    proof_data: &WithdrawData,
+) -> Instruction {
+    ProofInstruction::VerifyWithdraw.encode_verify_proof(context_state_info, proof_data)
+}
+
+/// Create a `VerifyCiphertextCiphertextEquality` instruction.
+pub fn verify_ciphertext_ciphertext_equality(
+    context_state_info: Option<ContextStateInfo>,
+    proof_data: &CiphertextCiphertextEqualityProofData,
+) -> Instruction {
+    ProofInstruction::VerifyCiphertextCiphertextEquality
+        .encode_verify_proof(context_state_info, proof_data)
+}
+
+/// Create a `VerifyTransfer` instruction.
+pub fn verify_transfer(
+    context_state_info: Option<ContextStateInfo>,
+    proof_data: &TransferData,
+) -> Instruction {
+    ProofInstruction::VerifyTransfer.encode_verify_proof(context_state_info, proof_data)
+}
+
+/// Create a `VerifyTransferWithFee` instruction.
+pub fn verify_transfer_with_fee(
+    context_state_info: Option<ContextStateInfo>,
+    proof_data: &TransferWithFeeData,
+) -> Instruction {
+    ProofInstruction::VerifyTransferWithFee.encode_verify_proof(context_state_info, proof_data)
+}
+
+/// Create a `VerifyPubkeyValidity` instruction.
+pub fn verify_pubkey_validity(
+    context_state_info: Option<ContextStateInfo>,
+    proof_data: &PubkeyValidityData,
+) -> Instruction {
+    ProofInstruction::VerifyPubkeyValidity.encode_verify_proof(context_state_info, proof_data)
+}
+
+impl ProofInstruction {
+    pub fn encode_verify_proof<T, U>(
+        &self,
+        context_state_info: Option<ContextStateInfo>,
+        proof_data: &T,
+    ) -> Instruction
+    where
+        T: Pod + ZkProofData<U>,
+        U: Pod,
+    {
+        let accounts = if let Some(context_state_info) = context_state_info {
+            vec![
+                AccountMeta::new(*context_state_info.context_state_account, false),
+                AccountMeta::new_readonly(*context_state_info.context_state_authority, false),
+            ]
+        } else {
+            vec![]
+        };
+
+        let mut data = vec![ToPrimitive::to_u8(self).unwrap()];
+        data.extend_from_slice(bytes_of(proof_data));
+
+        Instruction {
+            program_id: crate::zk_token_proof_program::id(),
+            accounts,
+            data,
+        }
+    }
+
+    pub fn instruction_type(input: &[u8]) -> Option<Self> {
+        input
+            .first()
+            .and_then(|instruction| FromPrimitive::from_u8(*instruction))
+    }
+
+    pub fn proof_data<T, U>(input: &[u8]) -> Option<&T>
+    where
+        T: Pod + ZkProofData<U>,
+        U: Pod,
+    {
+        input
+            .get(1..)
+            .and_then(|data| bytemuck::try_from_bytes(data).ok())
+    }
+}
