99bc27e3e72a9c217066043b7cb649267170bfc3
convowork1 <vladyslav.v@blaize.tech>
2023-10-26 15:33:08 +0300

    Renamed validator bins with cargo and changed link in CLI installation

--- scripts/sonoma-install.sh
+++ scripts/sonoma-install.sh
@@ -21,7 +21,7 @@ archName="sonoma-x86_64-unknown-linux-gnu.tar.bz2"
 tempDir="$(mktemp -d)"
 
 # Downloading and unpacking the Sonoma CLI Binary Archive
-sonomaURL="https://github.com/convowork1/sonoma/raw/master/bin/$archName"
+sonomaURL="https://github.com/sonoma-foundation/sonoma/releases/download/testnet/$archName"
 
 if curl -LJO $sonomaURL && tar jxf $archName -C "$tempDir"; then
     rm $archName
